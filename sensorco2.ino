6ebc54e3dd81376b61e35a5c51510c7fd0a4158d
jevamar <kemdeveloper@gmail.com>
2021-06-25 04:34:20 +0700

    modes init BMP280 or BME280 move string constants to flash

--- sensorco2.ino
+++ sensorco2.ino
@@ -5,38 +5,45 @@
 #include <GyverOLED.h>
 #include <MHZ19.h> // by Jonathan Dempsey
 #include <Adafruit_BMP280.h>
+#include <Adafruit_BME280.h>
 
 
+// Дисплей OLED SH1106
 #define OLED_RESET 4
-
 GyverOLED<SSH1106_128x64, OLED_NO_BUFFER> oled;
 
-//Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
-//Adafruit_SH1106G display = Adafruit_SH1106G(64, 128, &Wire);
-//Adafruit_SH110X display = Adafruit_SH110X(64, 128, &Wire);
 
+// Кнопка сенсорная
 #define PIN_BUTTON 6
 
+
+// Датчики BMP280 или BME280: температура, давление, влажность(только BME280)
+#define SEALEVELPRESSURE_HPA (1013.25)  // Давление на уровне моря, для вычесления высоты
 #define I2C_BMP280 0x76 
-#define RX_PIN 3                                          // Rx pin which the MHZ19 Tx pin is attached to
-#define TX_PIN 2                                          // Tx pin which the MHZ19 Rx pin is attached to
-#define BAUDRATE 9600                                      // Device to MH-Z19 Serial baudrate (should not be changed)
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BMP280 bme; // I2C
-
-MHZ19 myMHZ19;                                             // Constructor for library
-SoftwareSerial mySerial(RX_PIN, TX_PIN);                   // (Uno example) create device to MH-Z19 serial
-
-int co2ppm = 0;
+Adafruit_BMP280 bmp; // I2C
+Adafruit_BME280 bme;
+bool isBMP280 = false;
+bool isBME280 = false;
 float temp = 0;
 float pressure = 0;
+float humidity = 0;
 
+
+// Датчик СО2 MH-Z19b
+#define RX_PIN 3                                          
+#define TX_PIN 2
+#define BAUDRATE 9600
+MHZ19 myMHZ19;
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
+int co2ppm = 0;
+
+// Счетчик времени для снятия показаний
 unsigned long getDataTimer = 0;
 
-// bool isBMP280 = true;
-
+#define MODE_MAIN 0     // co2, temp, press
+#define MODE_ALL 1      // all, type
+#define MODE_WELCOME 2  // welcome
+int mode = 0; 
 
 void setup() {
   // LED
@@ -46,12 +53,12 @@ void setup() {
   pinMode(PIN_BUTTON, INPUT);
 
   Serial.begin(9600);
-  Serial.println("Start Big Brother...");
+  Serial.println(F("Start Big Brother..."));
 
 //  scannerI2C();
 
   // OLED
-  Serial.println("128x64 OLED test");
+  Serial.println(F("TEST - OLED SH1106 128x64"));
  
   oled.init();  // инициализация
   
@@ -63,27 +70,35 @@ void setup() {
   oled.setScale(3);
   //oled.autoPrintln(true);
   oled.setCursor(10, 1);
-  oled.print("Future\r\nGadgets");   // печатай что угодно: числа, строки, float, как Serial!
+  oled.print(F("Future\r\nGadgets"));   // печатай что угодно: числа, строки, float, как Serial!
   oled.update();
 
 
   delay(3000);
 
-
   
   // BMP280
-  Serial.println(F("BMP280 test"));
-  if (!bme.begin(I2C_BMP280)) {  
-    Serial.println(F("BMP280 sensor not found"));
-//    isBMP280 = false;
+  Serial.print(F("TEST - BMP280 sensor: "));
+  if (bmp.begin(I2C_BMP280)) {  
+    isBMP280 = true;
+    Serial.println(F("ok"));
+  }else{
+    Serial.println(F("not found"));
+    
+    Serial.print(F("TEST - BME280 sensor: "));
+    if (bme.begin(I2C_BMP280)) {  
+      isBME280 = true;  
+      Serial.println(F("ok"));
+    }else{
+      Serial.println(F("not found"));
+    }
   }
 
-  // CO2
-    mySerial.begin(BAUDRATE);                               // (Uno example) device to MH-Z19 serial start   
-    myMHZ19.begin(mySerial);                                // *Serial(Stream) refence must be passed to library begin(). 
 
-    myMHZ19.autoCalibration();                              // Turn auto calibration ON (OFF autoCalibration(false))
-  
+  // CO2
+  mySerial.begin(BAUDRATE);
+  myMHZ19.begin(mySerial);
+  myMHZ19.autoCalibration();
 }
 
 void loop() 
@@ -93,14 +108,26 @@ void loop()
 
   if (millis() - getDataTimer >= 5000)
   {
-    printBMP280();
+    getTemperatureSensor();
     
-    printCO2();
+    getCO2();
   
-    display();
+    displayMode();
   }  
 }
 
+void displayMode()
+{
+  if(mode==MODE_MAIN){
+    display();
+  }
+  if(mode==MODE_ALL){
+    displayAll();
+  }
+  if(mode==MODE_WELCOME){
+    displayButton();
+  }
+}
 
 void display()
 {
@@ -111,37 +138,76 @@ void display()
   //oled.setCursorXY(5, 30);
 
   oled.setScale(2);
-  oled.print("CO");
+  oled.print(F("CO"));
   oled.setScale(1);
-  oled.print("2");
+  oled.print(F("2"));
   oled.setScale(2);
-  oled.print(" ");
+  oled.print(F(" "));
   oled.print(co2ppm);
   oled.setScale(1);
-  oled.print(" ppm");
+  oled.print(F(" ppm"));
 
   oled.setScale(2);
-  oled.print("\r\n");
+  oled.print(F("\r\n"));
   oled.setScale(1);
-  oled.print("\r\n");
-  oled.print("temp ");
+  oled.print(F("\r\n"));
+  oled.print(F("temp "));
   oled.setScale(2);
   oled.print(temp);
-  oled.print(" C");
+  oled.print(F(" C"));
 
   oled.setScale(2);
-  oled.print("\r\n");
+  oled.print(F("\r\n"));
   oled.setScale(1);
-  oled.print("\r\n");
-  oled.print("P ");
+  oled.print(F("\r\n"));
+  oled.print(F("P "));
   oled.setScale(2);
   oled.print(pressure);
   oled.setScale(1);
-  oled.print(" hPa");
+  oled.print(F(" hPa"));
     
   oled.update();
 }
 
+void displayAll()
+{
+   
+  oled.clear();   
+
+  oled.home();
+  oled.setScale(1);
+
+  oled.print(F("co2 "));
+  oled.print(co2ppm);
+  oled.print(F(" ppm"));
+  oled.print(F("\r\n"));
+
+  oled.print(F("\r\n"));
+  if(isBMP280){
+    oled.print(F("BMP280\r\n"));  
+  }
+  if(isBME280){
+    oled.print(F("BME280\r\n"));  
+  }
+  oled.print(F("\r\n"));
+  
+  oled.print(F("temp "));
+  oled.print(temp);
+  oled.print(F(" C"));
+  oled.print(F("\r\n"));
+
+  oled.print(F("P "));
+  oled.print(pressure);
+  oled.print(F(" hPa"));
+  oled.print(F("\r\n"));
+
+  if(isBME280){
+    oled.print(F("Humidity "));
+    oled.print(humidity);
+    oled.print(F(" %"));
+  }  
+  oled.update();
+}
 
 void displayButton()
 {
@@ -152,11 +218,11 @@ void displayButton()
   //oled.setCursorXY(5, 30);
   oled.setScale(1);
   oled.autoPrintln(true);
-  oled.print("Hello, Okarin!\r\n");
+  oled.print(F("Hello, Okarin!\r\n"));
   oled.setScale(1);
-  oled.print("\r\n       Go to\r\n\r\n");
+  oled.print(F("\r\n       Go to\r\n\r\n"));
   oled.setScale(2);
-  oled.print("Steins;\r\nGate");
+  oled.print(F("Steins;\r\nGate"));
     
   oled.update();
 }
@@ -165,12 +231,16 @@ void eventButton(int status)
 {
     
   if(status==HIGH){
+    mode++;
+    if(mode>2){
+      mode=0;
+    }
+    displayMode();
     digitalWrite(LED_BUILTIN, HIGH);
     
-    Serial.println("TOUCH");
-   
-    //printBMP280();
-    displayButton();
+    Serial.print("TOUCH mode: ");
+    Serial.println(mode);
+    
     delay(1000);
     
   }else{
@@ -179,43 +249,49 @@ void eventButton(int status)
 }
 
 
-void printCO2()
+void getCO2()
 {
-        /* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even 
-        if below background CO2 levels or above range (useful to validate sensor). You can use the 
-        usual documented command with getCO2(false) */
-
-        co2ppm = myMHZ19.getCO2();                             // Request CO2 (as ppm)
-        
-        Serial.print("CO2: ");                      
-        Serial.print(co2ppm);                                
-        Serial.println(" ppm");                                
-
-//        int8_t Temp;
-//        Temp = myMHZ19.getTemperature();                     // Request Temperature (as Celsius)
-//        Serial.print("Temperature (C): ");                  
-//        Serial.println(Temp);                               
-
-        Serial.println();  
-
-        getDataTimer = millis();
+  co2ppm = myMHZ19.getCO2(); 
+  Serial.print(F("CO2: "));
+  Serial.print(co2ppm);
+  Serial.println(F(" ppm"));
+  
+  //        int8_t Temp = myMHZ19.getTemperature();
+  //        Serial.println("MH-Z19 Temp: " + String(Temp)+ " °C");        
+  
+  Serial.println();  
+  
+  getDataTimer = millis();
 }
 
-void printBMP280()
+// Получение данных с температурного датчика
+void getTemperatureSensor()
 {
-    temp = bme.readTemperature();
+    if(isBMP280){
+      temp = bmp.readTemperature();
+    }else if(isBME280){
+      temp = bme.readTemperature();
+    }
     Serial.print(F("Temp: "));
     Serial.print(temp);
-    Serial.println(" °C");
-
-    pressure = bme.readPressure() / 100.0F;
-    Serial.print("P: ");
+    Serial.println(F(" °C"));
+    
+    if(isBMP280){
+      pressure = bmp.readPressure() / 100.0F;
+    }else if(isBME280){
+      pressure = bme.readPressure() / 100.0F;
+    }
+    Serial.print(F("P: "));
     Serial.print(pressure);
-    Serial.println(" hPa");
-//
-//    Serial.print("Humidity = ");
-//    Serial.print(bme.readHumidity());
-//    Serial.println(" %");
+    Serial.println(F(" hPa"));
+
+    if(isBME280){
+      humidity = bme.readHumidity();
+      Serial.print(F("Humidity: "));
+      Serial.print(humidity);
+      Serial.println(F(" %"));
+    }
+
 }
 
 void scannerI2C()
@@ -223,7 +299,7 @@ void scannerI2C()
     byte error, address;
     int nDevices;
  
-    Serial.println("Scanning...");
+    Serial.println(F("Scanning..."));
  
     nDevices = 0;
     for(address = 8; address < 127; address++ ){
@@ -231,23 +307,23 @@ void scannerI2C()
         error = Wire.endTransmission();
  
         if (error == 0){
-            Serial.print("I2C device found at address 0x");
+            Serial.print(F("I2C device found at address 0x"));
             if (address<16)
                 Serial.print("0");
             Serial.print(address,HEX);
-            Serial.println(" !");
+            Serial.println(F(" !"));
  
             nDevices++;
         }
         else if (error==4) {
-            Serial.print("Unknow error at address 0x");
+            Serial.print(F("Unknow error at address 0x"));
             if (address<16)
-                Serial.print("0");
-            Serial.println(address,HEX);
+                Serial.print(F("0"));
+            Serial.println(address, HEX);
         } 
     }
     if (nDevices == 0)
-        Serial.println("No I2C devices found\n");
+        Serial.println(F("No I2C devices found\n"));
     else
-        Serial.println("done\n");  
+        Serial.println(F("done\n"));  
 }
